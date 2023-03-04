bde284b0815dad0046928793e3fca2d3088d6fdb
Storefront <storefront@gmail.com>
2023-03-04 17:47:07 +0530

    new time0004

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 #Transform
 #Send email
 #Pull data from db
 
-def calculate():
-    x = 1
-    y = 2
-    return x
-
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'ajmal'})
+    queryset = Product.objects.filter(unit_price__range=(20,30))
+    return render(request, 'hello.html', {'name': 'ajmal', 'products': list(queryset)})
 
