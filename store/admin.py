4bb8082bc3f47cb9b4899a3ac63b636b982aac9e
Storefront <storefront@gmail.com>
2023-03-07 10:50:12 +0530

    new time0007

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory<10:
+            return 'LOW'
+        return 'OK'
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
 
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
+# admin.site.register(models.Product, ProductAdmin)
