181b56c020f08d4763d7e8d71e98960f85d0ab5b
Storefront <storefront@gmail.com>
2023-03-07 18:04:43 +0530

    new time0009

--- store/admin.py
+++ store/admin.py
@@ -1,15 +1,37 @@
-from django.contrib import admin
-from . import models
+from django.contrib import admin, messages
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode 
+from . import models
 
 # Register your models here.
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+    
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)    
+            
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
     
     @admin.display(ordering='products_count')
     def products_count(self,collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist')
+            +'?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            })
+        )
+        return format_html('<a href={}>{}</a>', url, collection.products_count)
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
             products_count = Count('product')
@@ -17,9 +39,11 @@ class CollectionAdmin(admin.ModelAdmin):
  
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_select_related = ['collection']
     
     def collection_title(self, product):
@@ -30,12 +54,22 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory<10:
             return 'LOW'
         return 'OK'
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated',
+            messages.SUCCESS
+        )
+    
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
  
 @admin.register(models.Order)   
 class OrderAdmin(admin.ModelAdmin):
