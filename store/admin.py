5ee9b8e723da4ae87f73ca4b1a9945970944dc3d
Storefront <storefront@gmail.com>
2023-03-07 11:10:40 +0530

    new time0008

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,13 @@ from . import models
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_title(self, product):
+        return product.collection.title
     
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -19,6 +23,9 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+    
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
 
 admin.site.register(models.Collection)
 # admin.site.register(models.Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -48,7 +48,15 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
+    
     
     
 class Order(models.Model):
@@ -64,6 +72,13 @@ class Order(models.Model):
     placed_at = models.DateField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
+    
  
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
