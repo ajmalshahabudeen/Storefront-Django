05033ff2543ad107e5a22ab0fedf61c79210bef5
Storefront <storefront@gmail.com>
2023-03-07 19:49:09 +0530

    new time0009

--- store/admin.py
+++ store/admin.py
@@ -21,6 +21,8 @@ class InventoryFilter(admin.SimpleListFilter):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title'] 
+
     
     @admin.display(ordering='products_count')
     def products_count(self,collection):
@@ -40,6 +42,12 @@ class CollectionAdmin(admin.ModelAdmin):
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     actions = ['clear_inventory']
+    search_fields = ['product']
+    autocomplete_fields = ['collection']
+    #exclude = ['promotions']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
@@ -71,8 +79,17 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
  
+class OrderItemInline(admin.TabularInline):
+     model = models.OrderItem
+     autocomplete_fields = ['product']
+     min_num = 1
+     max_num =10
+     extra =0
+ 
 @admin.register(models.Order)   
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
     list_display = ['id', 'placed_at', 'customer']
 
 
