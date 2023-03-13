95816336048b8e1db45c2f4d27a513f1d998dba8
Storefront <storefront@gmail.com>
2023-03-13 15:06:34 +0530

    serializing 0005 - Data validation method 02

--- store/views.py
+++ store/views.py
@@ -16,11 +16,9 @@ def product_list(request):
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.validated_data
-            return Response('ok')
-        else:
-            return Response(serializer.errors, '404')
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data
+        return Response('ok')
 
 @api_view()
 def product_detail(request, id):
