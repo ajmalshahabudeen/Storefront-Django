c22e0848e547b1612981d816a33498e58e6341b5
Storefront <storefront@gmail.com>
2023-03-13 11:57:22 +0530

    serializing 0004 - serializing relationship - method 02

--- store/serializers.py
+++ store/serializers.py
@@ -9,10 +9,7 @@ class ProductSerializer(serializers.Serializer):
     price_with_tax = serializers.SerializerMethodField(
         method_name= 'calculate_tax'
     )
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset = Collection.objects.all()
-    )
-    
+    collection = serializers.StringRelatedField()
     
     def calculate_tax(self, product:Product):
         return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
+    queryset = Product.objects.select_related('collection').all()
     serializer = ProductSerializer(queryset, many=True)
     return Response(serializer.data)
 
