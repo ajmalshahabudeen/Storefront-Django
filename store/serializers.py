aba093358d5093cd156592401461be0c7784392c
Storefront <storefront@gmail.com>
2023-03-13 12:37:08 +0530

    serializing 0005 - model serializing

--- store/serializers.py
+++ store/serializers.py
@@ -6,17 +6,22 @@ class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    price_with_tax = serializers.SerializerMethodField(
-        method_name= 'calculate_tax'
-    )
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name='collection-detail'
-    )
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'collection'] 
+
+ 
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    # price_with_tax = serializers.SerializerMethodField(
+    #     method_name= 'calculate_tax'
+    # )
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
     
     def calculate_tax(self, product:Product):
         return product.unit_price * Decimal(1.1)
