a7218ae4b07c4632f8c72954a1741e563c6364f4
Storefront <storefront@gmail.com>
2023-03-14 15:10:50 +0530

    serializing 0011 - View Sets - 002

--- store/views.py
+++ store/views.py
@@ -4,24 +4,19 @@ from django.db.models.aggregates import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
-
-class ProductList(ListCreateAPIView):
-    
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
     def get_serializer_context(self):
         return {'request':self.request}
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
     
     def delete(self, request, id): 
         product = get_object_or_404(Product, pk=id)       
@@ -33,16 +28,12 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class CollectionList(ListCreateAPIView):
+
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
             products_count=Count('products')).all()
     serializer_class = CollectionSerializer
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(
-            products_count=Count('products')).all()
-    serializer_class = CollectionSerializer
-
+    
     def delete(self, request, pk):
         collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0: # related_name='orderitems' in models.py under OrderItem class product object 
