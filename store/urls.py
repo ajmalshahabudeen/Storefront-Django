e745543a1fbc667acc902c3068552383c24472d2
Storefront <storefront@gmail.com>
2023-03-14 15:29:38 +0530

    serializing 0011 - Router in urls.py - 001

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,19 @@
 from . import views
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from pprint import pprint
 
-urlpatterns = [
-    path ('products/', views.ProductList.as_view()),
-    path ('products/<int:pk>/', views.ProductDetail.as_view()),
-    path ('collections/', views.CollectionList.as_view()),
-    path ('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+pprint(router.urls)
+
+urlpatterns = router.urls
+
+# [
+#     path ('products/', views.ProductList.as_view()),
+#     path ('products/<int:pk>/', views.ProductDetail.as_view()),
+#     path ('collections/', views.CollectionList.as_view()),
+#     path ('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
+# ]
  
