9bab42acf7cc70673efce6a654806c0fdfd3c306
Storefront <storefront@gmail.com>
2023-03-14 12:56:45 +0530

    serializing 0010 - generic views - using ListCreateAPIViews class - done for collectionlist class

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path ('products/', views.ProductList.as_view()),
     path ('products/<int:id>/', views.ProductDetail.as_view()),
-    path ('collections/', views.collection_list),
+    path ('collections/', views.CollectionList.as_view()),
     path ('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
 ]
  
--- store/views.py
+++ store/views.py
@@ -12,11 +12,9 @@ from .serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
 
 class ProductList(ListCreateAPIView):
-    def get_queryset(self):
-        return Product.objects.select_related('collection').all()
     
-    def get_serializer_class(self):
-        return ProductSerializer
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
     
     def get_serializer_context(self):
         return {'request':self.request}
@@ -45,19 +43,10 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(
-            products_count=Count('products')).all() # related_name='products in models.py under Product class in collection object
-        serializer = CollectionSerializer(
-            queryset, many=True, context ={'request': request}) 
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED) 
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(
+            products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
 
 @api_view(['GET', 'PUT', 'DELETE']) 
 def collection_detail(request, pk):
