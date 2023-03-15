88738f35efbb9fa9484fc5cc4331c2982e1be017
Storefront <storefront@gmail.com>
2023-03-15 17:18:23 +0530

    pagination with glocal - custom pagination

--- store/views.py
+++ store/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from .models import Product, Collection, OrderItem, Review
 from .filters import ProductFilter
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .pagination import DefaultPagination
 
 # Create your views here.
 class ProductViewSet(ModelViewSet):
@@ -16,7 +16,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
-    pagination_class = PageNumberPagination
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description'] 
     ordering_fields = ['unit_price', 'last_update']
     
--- storefront/settings.py
+++ storefront/settings.py
@@ -141,6 +141,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    # 'PAGE_SIZE': 10
     
 }
