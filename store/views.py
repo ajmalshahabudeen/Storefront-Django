6cc234e197683d13566d665cf5100e8693fb1127
Storefront <storefront@gmail.com>
2023-03-15 17:03:30 +0530

    pagination with global

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
@@ -12,8 +14,11 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer 
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    #pagination_class = PageNumberPagination
+    search_fields = ['title', 'description'] 
+    ordering_fields = ['unit_price', 'last_update']
     
     def get_serializer_context(self):
         return {'request':self.request} 
--- storefront/settings.py
+++ storefront/settings.py
@@ -139,5 +139,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'COERCE_DECIMAL_TO_STRING': False
+    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+    
 }
