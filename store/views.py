4b2f86604a685ef80723fc4ac016a2ad73a214a3
Storefront <storefront@gmail.com>
2023-03-17 16:36:41 +0530

    Created cart API - Getting cart, Deleting, Getting item - optimized

--- store/views.py
+++ store/views.py
@@ -67,4 +67,6 @@ class CartItemViewSet(ModelViewSet):
     serializer_class = CartItemSerializer
     
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
+        return CartItem.objects \
+            .filter(cart_id=self.kwargs['cart_pk']) \
+            .select_related('product')
