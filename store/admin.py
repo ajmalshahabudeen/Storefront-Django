49ddc05ae0981f1c085db78124a79c41b20e07ee
Storefront <storefront@gmail.com>
2023-03-23 08:58:33 +0530

    created an user profile

--- store/admin.py
+++ store/admin.py
@@ -77,10 +77,12 @@ class ProductAdmin(admin.ModelAdmin):
     
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['user']
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     list_per_page = 10
-    ordering = ['first_name', 'last_name']
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
  
 class OrderItemInline(admin.TabularInline):
