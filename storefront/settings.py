d50e12155bf0b2b45a325e6e92077ef156738466
Ajmal <ajmalshahabudeen@gmail.com>
2023-04-10 11:14:52 +0530

    Installed Djoser and JWT Auth Enine

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
-    'rest_framework', 
+    'rest_framework',
+    'djoser', 
     'debug_toolbar',
     'playground',
     'store',
@@ -152,7 +153,14 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
 
 AUTH_USER_MODEL = 'core.User'
