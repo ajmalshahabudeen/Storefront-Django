1400295fe31398ff6e9b06048c00a773359347c4
Ajmal <ajmalshahabudeen@gmail.com>
2023-04-10 12:38:32 +0530

    Changed ACCESS token lifetime from 5 minutes to 1 day

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -169,4 +170,8 @@ DJOSER = {
     'SERIALIZERS':{
         'user_create': 'core.serializers.UserCreateSerializer'
     }
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
