812cfa345be444be3215580d93733d7befe98ac2
Ajmal <ajmalshahabudeen@gmail.com>
2023-04-25 19:17:49 +0530

    creating Order API - Applying permissions

--- store/views.py
+++ store/views.py
@@ -117,5 +117,13 @@ class CustomerViewSet(ModelViewSet):
         return Response('ok')
     
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
+    # queryset = Order.objects.all()
     serializer_class = OrderSerializers
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        customer_id = Customer.objects.only('id').get(user_id=user.id)
+        return Order.objects.filter(customer_id=customer_id)
