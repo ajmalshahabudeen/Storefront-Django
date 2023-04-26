30ad6e7db7baddbb4ad8808f6ba9ad153725e48b
Ajmal <ajmalshahabudeen@gmail.com>
2023-04-26 19:01:58 +0530

    creating Order API - created signal and modified get_or_create to get

--- store/serializers.py
+++ store/serializers.py
@@ -147,7 +147,7 @@ class CreateOrderSerializer(serializers.Serializer):
             # print(self.validated_data['cart_id'])
             # print(self.context['user_id'])
             
-            (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+            customer = Customer.objects.get(user_id=self.context['user_id'])
             order = Order.objects.create(customer=customer)
             #creating orders in cart
             cart_items = CartItem.objects \
