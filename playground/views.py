ccc7813007ec5772458aa538dbcd89cdb44049c1
Ajmal <ajmalshahabudeen@gmail.com>
2023-05-03 14:28:13 +0530

    Sending Emails -- attaching files used EmailMessage module

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # from django.db.models import Q 
 # from django.db.models.aggregates import Min,Max,Count,Avg 
 # from store.models import Product, OrderItem 
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+# from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 
 # Create your views here.
 #Transform
@@ -14,11 +15,19 @@ def say_hello(request):
     # result = Product.objects.aggregate(Count('id'),Min('unit_price'))
     # return render(request, 'hello.html', {'name': 'ajmal', 'result':result})
     try:
-        mail_admins(
+        message = EmailMessage(
             'subject',
-            'message',
-            html_message='message'
+            'messages',
+            'from@mail.com',
+            ['to@mail.com']
         )
+        message.attach_file('playground/static/images/Ajmal_bg.jpg')
+        message.send()
+        # mail_admins(
+        #     'subject',
+        #     'message',
+        #     html_message='message'
+        # )
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'ajmal'})
