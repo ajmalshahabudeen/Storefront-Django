adebba24037cc668bb1a5d17fceba96200fcb540
ajmaljarvis <ajmalshahabudeen@gmail.com>
2023-05-05 11:11:44 +0530

    "test_if_user_not_admin_returns_403" test created

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -17,3 +17,10 @@ class TestCreateCollection:
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
     
+    def test_if_user_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title':'a'})
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN   
+    
